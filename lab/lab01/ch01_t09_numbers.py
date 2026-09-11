aa1818d1a9d58b616bfa3831480e5d5ca7a55db9
HuangJingjing-2627 <wzing16@gmail.com>
2026-09-11 06:52:23 +0000

    [skip actions] lab/lab01/ch01_t09_numbers.py

--- lab/lab01/ch01_t09_numbers.py
+++ lab/lab01/ch01_t09_numbers.py
@@ -5,4 +5,4 @@ from tests.unit_test_helper.console_tset_helper import *
 class Test0utput(unittest.TestCase):
 
     def test(self):
-        temp_globals, temp_locals, content, output = execfile
+        temp_globals, temp_locals, content, output = execfile("")
