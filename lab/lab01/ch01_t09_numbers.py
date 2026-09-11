79f2607c653ed84945b09288e289b52106334921
HuangJingjing-2627 <wzing16@gmail.com>
2026-09-11 06:53:59 +0000

    [skip actions] lab/lab01/ch01_t09_numbers.py

--- lab/lab01/ch01_t09_numbers.py
+++ lab/lab01/ch01_t09_numbers.py
@@ -7,4 +7,4 @@ class Test0utput(unittest.TestCase):
     def test(self):
         temp_globals, temp_locals, content, output = execfile("lab01/ch01_t09_numbers.py")
         print(temp_locals)
-        self.assertAlmostEqual(1, temp_locals)
+        self.assertAlmostEqual(1, temp_locals{})
