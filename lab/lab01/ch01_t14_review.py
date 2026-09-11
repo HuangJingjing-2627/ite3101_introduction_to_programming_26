dc451201b2982bb076efe267fd799e1661203a0b
HuangJingjing-2627 <wzing16@gmail.com>
2026-09-11 08:10:49 +0000

    [skip actions] lab/lab01/ch01_t14_review.py

--- lab/lab01/ch01_t14_review.py
+++ lab/lab01/ch01_t14_review.py
@@ -1,2 +1,9 @@
 skill_completed = "Python Syntax"
-e
+exercises_completed = 13
+
+points_pet_exercise = 5
+
+point_total = 100
+point_total +=exercises_completed * points_per_exercise
+
+print("I got " + str(point_total))
